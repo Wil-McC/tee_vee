f1e75384993426183e1f1c40c64d2c4dd8d233be
Wil McCauley <wmccauley724@gmail.com>
2020-12-15 10:53:14 -0700

    Add passing tests for I4 Network methods

--- test/network_test.rb
+++ test/network_test.rb
@@ -10,6 +10,8 @@ class ShowTest < Minitest::Test
     @kitt           = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
     @michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
     @knight_rider   = Show.new("Knight Rider", "Glen Larson", [@michael_knight, @kitt])
+    @mitch          = Character.new({name: "Mitch Buchannon", actor: "David Hasselhoff", salary: 1_200_000})
+    @baywatch       = Show.new("Baywatch", "Gregory Bonann", [@mitch])
     @leslie_knope   = Character.new({name: "Leslie Knope", actor: "Amy Poehler", salary: 2_000_000})
     @ron_swanson    = Character.new({name: "Ron Swanson", actor: "Nick Offerman", salary: 1_400_000})
     @parks_and_rec  = Show.new("Parks and Recreation", "Michael Shur & Greg Daniels", [@leslie_knope, @ron_swanson])
@@ -45,5 +47,28 @@ class ShowTest < Minitest::Test
                     }
 
     assert_equal expected_hash, @nbc.actors_by_show
+  end
+
+  def test_it_returns_shows_by_actor
+    @nbc.add_show(@knight_rider)
+    @nbc.add_show(@baywatch)
+    @nbc.add_show(@parks_and_rec)
+
+    expected_hash = {
+                      "David Hasselhoff" => ["Knight Rider", "Baywatch"],
+                      "William Daniels" => ["Knight Rider"],
+                      "Amy Poehler" => ["Parks and Recreation"],
+                      "Nick Offerman" => ["Parks and Recreation"]
+                    }
+
+    assert_equal expected_hash, @nbc.shows_by_actor
+  end
+
+  def test_it_returns_prolific_actors
+    @nbc.add_show(@knight_rider)
+    @nbc.add_show(@baywatch)
+    @nbc.add_show(@parks_and_rec)
+
+    assert_equal ["David Hasselhoff"], @nbc.prolific_actors
   end
 end
