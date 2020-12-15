b0cca29ccfe94d50b910a3bd619deed55850d3d0
Wil McCauley <wmccauley724@gmail.com>
2020-12-15 10:08:24 -0700

    Add main characters and actors_by_show methods

--- lib/network.rb
+++ lib/network.rb
@@ -10,4 +10,20 @@ class Network
   def add_show(show)
     @shows.push(show)
   end
+
+  def main_characters
+    @shows.map do |show|
+      show.characters.select do |char|
+        char.salary > 500_000 && char.name.upcase == char.name
+      end
+    end.flatten
+  end
+
+  def actors_by_show
+    show_actors_hash = Hash.new
+    @shows.each do |show|
+      show_actors_hash[show] = show.actors
+    end
+    show_actors_hash
+  end
 end
