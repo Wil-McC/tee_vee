182d8fcf2a22ee9d794205deb47670f68c4e3812
Wil McCauley <wmccauley724@gmail.com>
2020-12-15 10:52:58 -0700

    Add methods for I4 interaction pattern

--- lib/network.rb
+++ lib/network.rb
@@ -26,4 +26,26 @@ class Network
     end
     show_actors_hash
   end
+
+  def shows_by_actor
+    actors = (@shows.map { |show| show.actors }.flatten).uniq
+    shows_by_actor_hash = Hash.new
+    actors.each do |actor|
+      @shows.each do |show|
+        if show.actors.include?(actor) && shows_by_actor_hash[actor]
+          shows_by_actor_hash[actor] << show.name
+        elsif show.actors.include?(actor)
+          shows_by_actor_hash[actor] = [show.name]
+        end
+      end
+    end
+    shows_by_actor_hash
+  end
+
+  def prolific_actors
+    actors = (@shows.map { |show| show.actors }.flatten).uniq
+    actors.select do |actor|
+      shows_by_actor[actor].length > 1
+    end
+  end
 end
